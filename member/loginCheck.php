fdb82b32703ef70c2ec829a68ec3f1c35e0f8248
JangJeaHun <wkdwogns2345@naver.com>
2022-01-07 14:31:11 +0900

    signin.html finish

---
+++ member/loginCheck.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    echo '<h1>입력한 정보</h1>';
+    echo $_POST['userId'];
+    echo '<br>';
+    echo $_POST['userPw'];
+    ?>
+
+    <?php
+    echo '<h1>2번째</h1>'
+    $uid = $_POST['userId'];
+    $upw = $_POST['userPw'];
+
+    if( echo($_POST['']))
+    
+
+    else
+
+    ?>
+
+
+</body>
+</html>
